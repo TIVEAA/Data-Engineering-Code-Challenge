26ab1814249137074ef08f2551fe5cd2956ac7e0
Vieira <tiago.a.vieira@cgi.com>
2025-03-29 16:55:28 +0000

    Adding new functions to data_preparation.py file

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,8 +1,11 @@
 from utils import load_config, get_logger
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.functions import col, when, count, length, to_date
+from pyspark.sql.types import StringType
+from pyspark.sql.functions import col, when, count, length, to_date, udf, to_timestamp
+from dateutil import parser
 
 spark = SparkSession.builder.appName("DataPreparation").getOrCreate()
+spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
 logger = get_logger(__name__)
 
 config = load_config("config/tables_config.json")
@@ -69,6 +72,16 @@ def check_format_inconsistencies(df: DataFrame, config: dict) -> DataFrame:
     return inconsistencies_df
 
 def handle_duplicates(df: DataFrame, config: dict) -> DataFrame:
+    """
+    Handle duplicates in the DataFrame by dropping rows with duplicate primary keys.
+
+    Args:
+        df (DataFrame): The DataFrame to process.
+        config (dict): The configuration dictionary for the table.
+
+    Returns:
+        DataFrame: The DataFrame with duplicates removed.
+    """
     logger.info(f"Handling duplicates in table {config['table_name']}")
     initial_count = df.count()
     df = df.drop_duplicates(subset=config['primary_keys'])
@@ -76,19 +89,81 @@ def handle_duplicates(df: DataFrame, config: dict) -> DataFrame:
     duplicates_dropped = initial_count - final_count
     logger.info(f"Dropped {duplicates_dropped} duplicates rows from table {config['table_name']}")
     return df
-        
-sales_df = load_data(spark, config['sales'])
-products_df = load_data(spark, config['products'])
-stores_df = load_data(spark, config['stores'])
 
-# products_df.show()
+def detect_date_format(date_str: str) -> str:
+    """
+    Detects and parses the date string into a standard format.
 
-missing_values = check_missing_values(sales_df, config['sales'])
-missing_values.show()
+    Args:
+        date_str (str): The date string to parse.
 
-format_inconsistencies = check_format_inconsistencies(sales_df, config['sales'])
-format_inconsistencies.show()
+    Returns:
+        str: The parsed date in ISO format or None if parsing fails.
+    """
+    try:
+        return parser.parse(date_str).isoformat()
+    except ValueError:
+        return None
 
-sales_df = handle_duplicates(sales_df, config['sales'])
+def enforce_constraints(df: DataFrame, column: str, constraints: dict) -> DataFrame:
+    """
+    Enforces constraints for a specific column in the DataFrame.
+
+    Args:
+        df (DataFrame): The DataFrame to process.
+        column (str): The column to enforce constraints on.
+        constraints (dict): The constraints dictionary for the column.
+
+    Returns:
+        DataFrame: The DataFrame with enforced constraints.
+    """
+    if 'not_null' in constraints and constraints['not_null']:
+        logger.info(f"Enforcing not_null constraint on column {column}")
+        df = df.filter(col(column).isNotNull())
+    if 'not_empty' in constraints and constraints['not_empty']:
+        logger.info(f"Enforcing not_empty constraint on column {column}")
+        df = df.filter(col(column) != "")
+    if 'min' in constraints:
+        logger.info(f"Enforcing min constraint on column {column}: {constraints['min']}")
+        df = df.filter(col(column) >= constraints['min'])
+    return df
+
+def enforce_data_types(df: DataFrame, config: dict) -> DataFrame:
+    """
+    Enforces data types and constraints for each column in the DataFrame.
+
+    Args:
+        df (DataFrame): The DataFrame to process.
+        config (dict): The configuration dictionary for the table.
+
+    Returns:
+        DataFrame: The DataFrame with enforced data types and constraints.
+    """
+    logger.info(f"Enforcing data types for table {config['table_name']}")
+    for column, metadata in config['columns'].items():
+        data_type = metadata['type']
+        logger.info(f"Enforcing data type for column {column}: {data_type}")
+        if data_type == 'date':
+            logger.info(f"Detecting and converting date formats for column {column}")
+            df = df.withColumn(column, detect_date_format_udf(col(column)))
+            date_format = str(metadata['format'])
+            df = df.withColumn(column, to_date(col(column), date_format))
+        else:
+            df = df.withColumn(column, col(column).cast(data_type))
+
+        if 'constraints' in metadata:
+            df = enforce_constraints(df, column, metadata['constraints'])
+    return df
+
+# sales_df = load_data(spark, config['sales'])
+# products_df = load_data(spark, config['products'])
+# stores_df = load_data(spark, config['stores'])
+
+# sales_df = handle_duplicates(sales_df, config['sales'])
+# detect_date_format_udf = udf(detect_date_format, StringType())
+# sales_df = enforce_data_types(sales_df, config['sales'])
+# sales_df.printSchema()
+# sales_df.show()
+# print(sales_df.count())
 
 spark.stop()
